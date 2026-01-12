c1d620bed7a2a05fab9391799208cafac966aeb5
sbutterl <sheila.butterly@ninetyone.com>
2026-01-12 17:06:30 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("er-game").then(cache =>
+      cache.addAll(["./","./index.html","./manifest.json","./icon.png"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
